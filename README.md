ca70c16cd6452844c053e77f6b4e319d8ed26e3c
Vitor Fernandes <vmlf01@gmail.com>
2016-05-07 14:55:04 +0100

    add presentation files

---
+++ README.md
@@ -0,0 +1,5 @@
+# [Rinse Repeat](http://slides.com/vitorfernandes/nodejs-rinse-repeat#/)
+
+## Summary
+The Node.js event loop and how it works.
+
---
+++ samples/index.js
@@ -0,0 +1,8 @@
+console.log('Hello World');
+
+// try this version to see the process
+// wait 5 seconds before terminating
+// -----------
+//setTimeout(function () {
+//    console.log('Hello World');
+//}, 5000);
